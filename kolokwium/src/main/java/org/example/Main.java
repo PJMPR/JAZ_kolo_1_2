39f82b6de11114c5f0e1b0d3091f4732d1daea8e
adam urbanowicz <adam.urbanow@gmail.com>
2022-11-20 17:24:59 +0100

    start

---
+++ kolokwium/src/main/java/org/example/Main.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import org.example.model.SampleUsers;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Zaczynamy. Życzę powodzenia :)");
+        var users = SampleUsers.listOfUsers;
+
+    }
+
+    public static double getAverageRatingOfActiveVipUsers()
+    {
+        return 0;
+    }
+
+    public static void deactivateInternalUsersLastSeenYearAgo(){}
+}
